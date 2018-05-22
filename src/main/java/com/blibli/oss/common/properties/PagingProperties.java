ddef8e581579064a16c49f19763656e65204079c
Bryan Arista <bryanarista@me.com>
2018-05-22 21:45:26 +0700

    Use lombok

--- src/main/java/com/blibli/oss/common/properties/PagingProperties.java
+++ src/main/java/com/blibli/oss/common/properties/PagingProperties.java
@@ -1,11 +1,17 @@
 package com.blibli.oss.common.properties;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
  * @author Eko Kurniawan Khannedy
  */
 @ConfigurationProperties("common.paging")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PagingProperties {
 
   private Integer defaultPage = 1;
@@ -21,60 +27,4 @@ public class PagingProperties {
   private String itemPerPageQueryParam = "item_per_page";
 
   private String sortByQueryParam = "sort_by";
-
-  public Integer getMaxItemPerPage() {
-    return maxItemPerPage;
-  }
-
-  public void setMaxItemPerPage(Integer maxItemPerPage) {
-    this.maxItemPerPage = maxItemPerPage;
-  }
-
-  public String getPageQueryParam() {
-    return pageQueryParam;
-  }
-
-  public void setPageQueryParam(String pageQueryParam) {
-    this.pageQueryParam = pageQueryParam;
-  }
-
-  public String getItemPerPageQueryParam() {
-    return itemPerPageQueryParam;
-  }
-
-  public void setItemPerPageQueryParam(String itemPerPageQueryParam) {
-    this.itemPerPageQueryParam = itemPerPageQueryParam;
-  }
-
-  public String getSortByQueryParam() {
-    return sortByQueryParam;
-  }
-
-  public void setSortByQueryParam(String sortByQueryParam) {
-    this.sortByQueryParam = sortByQueryParam;
-  }
-
-  public Integer getDefaultPage() {
-    return defaultPage;
-  }
-
-  public void setDefaultPage(Integer defaultPage) {
-    this.defaultPage = defaultPage;
-  }
-
-  public Integer getDefaultItemPerPage() {
-    return defaultItemPerPage;
-  }
-
-  public void setDefaultItemPerPage(Integer defaultItemPerPage) {
-    this.defaultItemPerPage = defaultItemPerPage;
-  }
-
-  public String getDefaultSortDirection() {
-    return defaultSortDirection;
-  }
-
-  public void setDefaultSortDirection(String defaultSortDirection) {
-    this.defaultSortDirection = defaultSortDirection;
-  }
 }
