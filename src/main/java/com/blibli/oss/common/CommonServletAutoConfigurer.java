46605b4dbc9237499dd3c6ffc40b605d873f7137
Adhika Setya Pramudita <adhika.pramudita@gdn-commerce.com>
2018-10-28 23:13:43 +0700

    Enable paging intercept on servlet only

--- src/main/java/com/blibli/oss/common/CommonServletAutoConfigurer.java
+++ src/main/java/com/blibli/oss/common/CommonServletAutoConfigurer.java
@@ -3,6 +3,7 @@ package com.blibli.oss.common;
 import com.blibli.oss.common.paging.PagingInterceptor;
 import com.blibli.oss.common.properties.PagingProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -13,7 +14,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Configuration
 @EnableConfigurationProperties({PagingProperties.class})
-public class CommonAutoConfigurer extends WebMvcConfigurerAdapter {
+@ConditionalOnClass(name = "org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter")
+public class CommonServletAutoConfigurer extends WebMvcConfigurerAdapter {
 
   @Autowired
   private PagingProperties pagingProperties;
